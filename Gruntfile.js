c252d2bee79f733e044ab2982187eae7dd6d3d47
Leonardo Quixada <lquixada@gmail.com>
2013-05-04 12:50:38 -0300

    simplifying Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,7 @@
 module.exports = function (grunt) {
 
 	grunt.initConfig({
-		projectName: 'litemq',
-
-	  jasmine: {
-			pivotal: {
-				src: '*.src.js',
-				options: {
-					host: 'http://localhost:9001/',
-					vendor: './vendor/pro.create-0.0.3.min.js',
-					specs: '*.spec.js',
-					outfile: 'runner.html'
-				}
-			}
-		}
+		projectName: 'litemq'
 	});
 
 	grunt.loadNpmTasks('grunt-o-bundle');
